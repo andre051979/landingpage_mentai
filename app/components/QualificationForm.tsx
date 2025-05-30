7d3b1b4317e470a33aca0f0d2f29d8228f11e6df
andre051979 <160554992+andre051979@users.noreply.github.com>
2025-05-30 14:52:07 -0300

    Update QualificationForm.tsx

--- app/components/QualificationForm.tsx
+++ app/components/QualificationForm.tsx
@@ -94,7 +94,7 @@ export default function QualificationForm({ isOpen, onClose }: QualificationForm
         "Dificuldade em escalar operações sem aumentar custos",
         "Processos manuais que consomem tempo da equipe",
       ],
-     Construção Civil: [
+     Imobiliario: [
         "Inconsistência na qualidade do atendimento",
         "Dificuldade em escalar operações sem aumentar custos",
         "Processos manuais que consomem tempo da equipe",
